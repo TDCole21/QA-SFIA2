80407cc848773c8d23fb5cb170c48f18633705a0
TDCole21 <thomas.cole@academytrainee.com>
2020-04-11 15:13:46 +0000

    consistency

---
+++ service_1/application/routes.py
@@ -0,0 +1,14 @@
+from application import app
+from flask import Flask, request, render_template, url_for, redirect
+from flask_mysqldb import MySQL
+import requests
+import random
+import os
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    response = requests.get('http://service_4:5003/randomword')
+    sentence = response.text
+    return render_template('index.html', sentence = sentence, title = 'Home')
