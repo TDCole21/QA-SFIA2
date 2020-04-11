0d4bc56d3bdd5069807997cc468b02b7c3105f54
TDCole21 <thomas.cole@academytrainee.com>
2020-04-11 13:36:13 +0000

    Docker stack

--- Service1/application/__init__.py
+++ Service1/application/__init__.py
@@ -6,9 +6,9 @@ import os
 app = Flask(__name__)
 
 #MySQl Config
-app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
-app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
-app.config['MYSQL_DB'] = os.environ.get('MYSQLDB')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
 from application import routes
