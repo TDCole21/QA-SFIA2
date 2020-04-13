612a1880d2277f01633a4364263ca415b22c8e0a
TDCole21 <thomas.cole@academytrainee.com>
2020-04-13 13:24:19 +0000

    jenkins

--- script/docker.sh
+++ script/docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl https://get.docker.com | sudo bash
 source ~/.bashrc
-sudo usermod -aG docker $USER
+sudo usermod -aG docker ${USER}
+su - ${USER}
 docker stack deploy --compose-file docker-compose.yml superherostack 
