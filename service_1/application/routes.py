d5e35ec3ab29811a747e02fa474cbcd47e82365e
TDCole21 <thomas.cole@academytrainee.com>
2020-04-13 18:44:22 +0000

    image update

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -12,3 +12,16 @@ def home():
     response = requests.get('http://service_4:5003/randomword')
     sentence = response.text
     return render_template('index.html', sentence = sentence, title = 'Home')
+
+@app.route('/generatename', methods=['GET', 'POST'])
+def home_create():
+    if request.method == "POST":
+        details=request.form
+        UsersName=details['name']
+        response = requests.get('http://service_4:5003/randomword')
+        SuperNames = response.text
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT IGNORE INTO SuperNames (UsersName, SuperNames) VALUES ((%s),(%s))", [UsersName, SuperNames])
+        mysql.connection.commit()
+        cur.close()
+        return render_template('index.html', sentence = SuperNames, title = 'Home')
