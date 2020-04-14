32527b92667c187eb48572d524cfc56678358cea
TDCole21 <thomas.cole@academytrainee.com>
2020-04-14 10:23:15 +0000

    readme

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -1,9 +1,22 @@
 from application import app
 import random
 
-
 @app.route('/randomphrase', methods=['GET', 'POST'])
 def beginning():
+
+	list = ['Super ','Bat ','Wonder ','The Amazing ','Dr. ', 'The Incredible ','Iron ']
+	
+	return list[random.randrange(6)]
+
+@app.route('/randomphrase/hero', methods=['GET', 'POST'])
+def beginning_hero():
+
+	list = ['Super ','Bat ','Wonder ','The Amazing ','Dr. ', 'The Incredible ','Iron ']
+	
+	return list[random.randrange(6)]
+
+@app.route('/randomphrase/villain', methods=['GET', 'POST'])
+def beginning_villain():
 
 	list = ['Super ','Bat ','Wonder ','The Amazing ','Dr. ', 'The Incredible ','Iron ']
 	
