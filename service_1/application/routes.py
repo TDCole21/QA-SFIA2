4ed856954b577d81b36567af01dddb5b48dffb31
TDCole21 <thomas.cole@academytrainee.com>
2020-04-14 10:36:37 +0000

    readme

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -11,16 +11,26 @@ mysql = MySQL(app)
 def home():
     response = requests.get('http://service_4:5003/randomword')
     sentence = response.text
+    names= open("names.txt", "a")
+    names.write(sentence)
+    names.close
     return render_template('index.html', sentence = sentence, title = 'Home')
 
 @app.route('/hero', methods=['GET', 'POST'])
 def home_hero():
     response = requests.get('http://service_4:5003/randomword/hero')
     sentence = response.text
+    names= open("names.txt", "a")
+    names.write(sentence)
+    names.close
     return render_template('index.html', sentence = sentence, title = 'Home')
 
 @app.route('/villain', methods=['GET', 'POST'])
 def home_villain():
     response = requests.get('http://service_4:5003/randomword/villain')
     sentence = response.text
+    names= open("names.txt", "a")
+    names.write(sentence)
+    names.close
     return render_template('index.html', sentence = sentence, title = 'Home')
+
